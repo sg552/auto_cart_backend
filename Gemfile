3cdf83bf98fa4b35f70933c7b1b73f9dbc3f9c59
Siwei <sg552sg552@gmail.com>
2014-07-26 18:20:53 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.16'
+
+gem 'sqlite3', '1.3.9'
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+gem 'jquery-rails', '3.1.1'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutoCartBackend::Application.config.secret_token = '45402be1fbaadb20ebf50dbf4688ac1cecb99b13b87cca7ed5aabb82c95de99b3f9b8fcfa9b88468c6d3adfb65d5bc859d17a0456f77e1a335945a8a68400d20'
