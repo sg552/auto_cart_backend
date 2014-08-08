255b8f4ba02da24fa776e1ce56a6a5a81c81413e
Siwei <sg552sg552@gmail.com>
2014-08-08 17:44:42 +0800

    encoding: utf-8

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 class NotebooksController < ApplicationController
   # GET /notebooks
   # GET /notebooks.json
--- db/migrate/20140726102323_create_notebooks.rb
+++ db/migrate/20140726102323_create_notebooks.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 class CreateNotebooks < ActiveRecord::Migration
   def change
     create_table :notebooks do |t|
