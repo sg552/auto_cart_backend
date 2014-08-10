c9af67a58d384b7d43b3a4a60daed8f610f7588d
Siwei <sg552sg552@gmail.com>
2014-08-10 10:35:20 +0800

    实现了最基本的rspec

--- lib/html_parser.rb
+++ lib/html_parser.rb
@@ -1,26 +1,26 @@
+require 'nokogiri'
 class HtmlParser
-  def self.parse html_content
-    page = Nokogiri::HTML(html_content)
-    item_descriptions = page.css('.itemDescription')
+  attr_accessor :page, :details_table, :price_table, :add_to_cart_button
+  def initialize html_content
+    @page = Nokogiri::HTML(html_content)
+    item_descriptions = @page.css('.itemDescription')
     item_descriptions.each_with_index do |item, i|
       return if i == 1
-
-      puts "-- parent_table: #{i}"
-      details_table = item.ancestors('table').first
+      @details_table = item.ancestors('table').first
       puts "====== details_table : #{details_table}"
       tr = details_table.ancestors('tr').first
       price_element = tr.css('.price').first
       puts "=== price: #{price_element.inspect}"
-      price_table = price_element.ancestors('table').first
+      @price_table = price_element.ancestors('table').first
       puts "====== price_table : #{price_table}"
-      add_to_cart_button = price_table.css('img[alt="Add to cart"]')
+      @add_to_cart_button = price_table.css('img[alt="Add to cart"]')
       puts "====== add_to_cart_button : #{add_to_cart_button}"
-
-
     end
   end
 
-  def self.get_price single_notebook_tr_dom
-    single_notebook_tr_dom.css('.price')
+  def real_price
+    result = @price_table.css('.price').first
+    puts "result: #{result}"
+    return result.text.sub '$', ''
   end
 end
--- spec/lib/html_parser_spec.rb
+++ spec/lib/html_parser_spec.rb
@@ -1,8 +1,22 @@
 require 'html_parser'
 describe HtmlParser do
 
-  it 'should parse html_content' do
-    puts 'hihihi'
+  before do
+    @parser = HtmlParser.new File.read('spec/fixtures/small_result.html')
   end
+
+  it 'should parse html_content' do
+    @parser.details_table.to_s.include?('<span class="itemDescription">Lenovo Lynx K3011</span>').
+      should == true
+    @parser.price_table.to_s.include?('<span class="strikeprice">$640.00</span><br><span class="price">$190.40</span>').
+      should == true
+    @parser.add_to_cart_button.to_s.include?('img src="//outlet.lenovo.com/SEUILibrary/lenovo-portal/images/icons/buttons/R5/add_to_cart.gif"').
+      should == true
+  end
+
+  it 'should get real_price' do
+    @parser.real_price.should == '190.40'
+  end
+
 
 end
