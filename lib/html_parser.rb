57a9c00107c2bc3ed959621249bbaecffab27be8
Siwei <sg552sg552@gmail.com>
2014-08-11 13:54:30 +0800

    大幅优化了解析速度

--- lib/html_parser.rb
+++ lib/html_parser.rb
@@ -10,15 +10,18 @@ class HtmlParser
     @page = get_content_with_pure_html html_content
     item_descriptions = @page.css('.itemDescription')
     @notebooks = []
+    puts "item_descriptions: #{item_descriptions.size}"
     item_descriptions.each_with_index do |item, i|
       return if options[:max_item].present? && i == options[:max_item]
       notebook = Notebook.new :created_by => @parser_id
-      @details_table = item.ancestors('table').first
-      tr = details_table.ancestors('tr').first
+      @details_table = item.xpath('./ancestor::table[1]')
+      #@details_table = item.ancestors('table').first
+      tr = details_table.xpath('./ancestor::tr[1]')
+      #tr = details_table.ancestors('tr').first
       set_attributes @details_table, notebook
 
       price_element = tr.css('.price').first
-      @price_table = price_element.ancestors('table').first
+      @price_table = price_element.xpath('./ancestor::table[1]')
       set_real_price @price_table, notebook
 
       @add_to_cart_button = price_table.css('img[alt="Add to cart"]')
@@ -50,10 +53,8 @@ class HtmlParser
 
   def get_content_with_pure_html html_content
     temp_html = Nokogiri::HTML(html_content)
-    puts "before remove, html_content: #{html_content.size}"
     temp_html.css('script').remove
     temp_html.css('style').remove
-    puts "after remove , html_content : #{temp_html.to_s.size}"
     temp_html
   end
   def set_add_to_cart_url add_to_cart_button, notebook
--- spec/controllers/notebook_filters_controller_spec.rb
+++ spec/controllers/notebook_filters_controller_spec.rb
@@ -6,10 +6,22 @@ describe NotebookFiltersController do
     post :read_complete_content, :html_content => File.read('spec/fixtures/small_result.html')
     response.body.include?('success').should == true
   end
-  it 'should post read_complete_content' do
+  it 'should post read_complete_content, 100 result' do
     filter = NotebookFilter.create :cpu => '2760', :screen => '1366', :price => '100-200'
     filter = NotebookFilter.create :cpu => '2760', :screen => 'ips', :price => '100-300'
-    post :read_complete_content, :html_content => File.read('spec/fixtures/big_content.html')
+    post :read_complete_content, :html_content => File.read('spec/fixtures/result_100.html')
+    response.body.include?('success').should == true
+  end
+  it 'should post read_complete_content, 200 result' do
+    filter = NotebookFilter.create :cpu => '2760', :screen => '1366', :price => '100-200'
+    filter = NotebookFilter.create :cpu => '2760', :screen => 'ips', :price => '100-300'
+    post :read_complete_content, :html_content => File.read('spec/fixtures/result_200.html')
+    response.body.include?('success').should == true
+  end
+  it 'should post read_complete_content, 500 result' do
+    filter = NotebookFilter.create :cpu => '2760', :screen => '1366', :price => '100-200'
+    filter = NotebookFilter.create :cpu => '2760', :screen => 'ips', :price => '100-300'
+    post :read_complete_content, :html_content => File.read('spec/fixtures/result_500.html')
     response.body.include?('success').should == true
   end
 end
